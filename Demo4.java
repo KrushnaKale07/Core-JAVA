ce6b382d786840228cee85fdc1b8c9c45aa70a1b
71603_Krushna <krushnakale0007@gmail.com>
2023-07-25 22:51:34 +0530

    Constructor Overloading

---
+++ Demo4.java
@@ -0,0 +1,12 @@
+class Demo4 
+{
+	public static void main(String[] args) 
+	{
+		Demo3 demo1=new Demo3();
+		Demo3 demo2=new Demo3(23);
+		Demo3 demo3=new Demo3('a');
+		Demo3 demo4=new Demo3(true);
+		Demo3 demo5=new Demo3("Strig");
+		System.out.println("Hello World!");
+	}
+}
