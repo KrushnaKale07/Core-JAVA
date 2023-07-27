763bc2c546dcfe0fa470c167fa8f30383ffd284d
71603_Krushna <krushnakale0007@gmail.com>
2023-07-27 23:45:28 +0530

    Looping

---
+++ src/com/krushna/looping/OddNumber.java
@@ -0,0 +1,27 @@
+package com.krushna.looping;
+
+import java.util.Scanner;
+public class OddNumber {
+	
+	public int test(int a)
+	{
+		return a;
+	}
+	
+	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Enter a number");
+//		int num = scanner.nextInt();
+		
+		OddNumber a = new OddNumber();
+		int num = a.test(4);
+		
+		if(num%2==0)
+		{
+			System.out.println(num+" is a even number");
+		}
+		else {
+			System.out.println(num+" is a odd number");
+		}
+	}
+}
